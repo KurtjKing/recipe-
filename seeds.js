692c4916b3e5140adca8583f43ce846b91556319
Kurt King <kurtking@Kurts-MacBook-Pro.local>
2019-06-10 18:30:31 -0500

    passport

---
+++ seeds.js
@@ -0,0 +1,44 @@
+var mongoose = require ("mongoose");
+var Campground = require("./models/users")
+
+
+var data =[
+    {username:"brad",
+    password:"dick",
+},
+{username:"samn",
+password:"fuck",
+},
+{username:"seed mtn",
+password:"you",
+}
+]
+
+function seedDB(){
+    Campground.deleteMany({},function(err){
+        if(err){
+            console.log(err);
+        }
+        console.log("removed users");
+        data.forEach(function(seed){
+            Campground.create(seed,function(err,campground){
+                if(err){
+                    console.log(err);
+                }else{
+                    console.log("added a user");
+                   
+
+                
+                        
+                      
+                    
+                }
+            });
+        });
+    });
+   
+   
+}
+
+
+module.exports = seedDB;
--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 var express = require("express");
 var mongoose = require("mongoose");
 var app = express();
+var passport = require("passport");
+var localStrategy = require("passport-local");
+var User = require("./models/users");
+var seedDB = require("./seeds");
+
+
+
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public'));
+app.use(require("express-session")({
+  secret:"keep it like",
+  resave:false,
+  saveUninitialized:false
+}));
 
-// mongoose.connect( "mongodb://localhost/recipe",{useNewUrlParser: true});
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+mongoose.connect( "mongodb://localhost/usersbase",{useNewUrlParser: true});
+
+seedDB();
 
 var PORT = process.env.PORT || 3000;
 
@@ -12,7 +32,7 @@ app.listen(PORT, function () {
     console.log("App running on port " + PORT + "!");
   });
 
-
+  
 
   app.get("/", function(req,res){
     res.render("landing");
@@ -23,5 +43,23 @@ app.listen(PORT, function () {
     res.render("register");
   });
 
+  // app.post("/register",function(req,res){
+  //   res.send("this is a post route");
+  // });
+
+
+  app.post("/register",function(req,res){
+    var newUser = new User({username:req.body.username});
+    User.register(newUser,req.body.password,function(err,user){
+        if(err){
+            console.log(err);
+           return res.render("register");
+        } else{
+           passport.authenticate("local")(req,res,function(){
+            res.redirect("/");
+           }); 
+        }
+    })
+});
 
   
